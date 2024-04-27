84e485efcb3ed973c5b9996fae7825fb03062a7f
Maxim <maxim.zubarev@icloud.com>
2024-04-27 12:06:07 +0400

    FIRST BLOOD

---
+++ README.md
@@ -0,0 +1,29 @@
+Google doesn't like you any more? While you are trying to fix the mess, make sure you make the most out of all the other search engines!
+
+Instant, low-config indexing script that submits the URLs in your sitemaps to **_Bing_** (and thereby **_DuckDuckGo_**), **_Naver_**, **_Seznam.cz_**, **_Yandex_**, and **_Yep_** for indexing.
+
+# 1. Quickstart
+
+## 1.1 Install
+
+```sh
+$ git clone https://github.com/mxmzb/indexnow-indexing-script
+$ cd ./indexnow-indexing-script
+$ yarn
+# OR
+$ npm install
+```
+
+## 1.2 Configure
+
+1. Head over to [https://api-keygen.com/](https://api-keygen.com/) and generate any key (I personally prefer 32 character hex).
+2. Create a file named `example.com/[your-generated-api-key].txt` for the domain that you want to index. In the file should, again, be the API key, and only the generated API key
+3. Rename `./config/example.yaml` to `[your-project-identifier].yaml` and change the values inside based on the explanations in comments
+
+## 1.3 Run
+
+```sh
+$ yarn index
+# OR
+$ npm run index
+```
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import fs from "fs";
+import path from "path";
+import ky from "ky";
+
+import { parse } from "yaml";
+import { XMLParser } from "fast-xml-parser";
+
+const MAX_REQUESTS_PER_MINUTE = 50;
+const xmlParser = new XMLParser();
+
+const sleep = () => new Promise((r) => setTimeout(r, (60 * 1000) / MAX_REQUESTS_PER_MINUTE));
+
+const getConfigs = async () =>
+  new Promise<string[]>((resolve) => {
+    const directoryPath = path.join(__dirname, "../config");
+
+    const configs: string[] = [];
+
+    fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+      files.forEach((file) => {
+        if (file.isFile() && path.extname(file.name) === ".yaml") {
+          configs.push(path.join("../config", file.name));
+        }
+      });
+
+      return resolve(configs);
+    });
+  });
+
+const chunkArray = <T>(array: T[], chunkSize: number = 1000): T[][] => {
+  const chunks: T[][] = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+};
+
+const getUrlChunksFromSitemap = async (sitemapUrl) => {
+  const res = await ky.get(sitemapUrl);
+  const xml = await res.text();
+  const parsedSitemap = xmlParser.parse(xml);
+  const urls = parsedSitemap?.urlset?.url;
+
+  if (Array.isArray(urls)) {
+    const urlChunks = chunkArray(
+      urls.map(({ loc }) => loc),
+      1000,
+    );
+
+    return urlChunks;
+  }
+};
+
+const indexSite = async (config) => {
+  config.sitemaps.forEach(async (sitemapUrl) => {
+    const urlChunks = await getUrlChunksFromSitemap(sitemapUrl);
+
+    if (Array.isArray(urlChunks)) {
+      for (let i = 0; i < urlChunks.length; i++) {
+        const chunk = urlChunks[i];
+
+        try {
+          const res = await ky.post(`https://api.indexnow.org/indexnow`, {
+            json: {
+              host: config.host,
+              key: config.api_key,
+              urlList: chunk,
+            },
+          });
+
+          if (res.status === 200) {
+            console.log(chunk.map((url) => `✅ Successfully submitted ${url}`).join("\n"));
+          }
+
+          await sleep();
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+  });
+};
+
+const run = async () => {
+  const configs = await getConfigs();
+
+  configs.forEach(async (configPath) => {
+    const config = parse(fs.readFileSync(path.join(__dirname, configPath), "utf8"));
+
+    await indexSite(config);
+  });
+};
+
+run();
